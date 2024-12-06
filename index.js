444a858f35177073640bde604273d454b135346b
sbrinkmeyer <sbrinkmeyer@gmail.com>
2024-12-05 18:16:18 -0600

    traversal

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 require("dotenv").config();
 
-const fs = require("fs/promises");
-const path = require("path");
-const axios = require("axios");
+const {traverseDirectory} = require("./src/traversal");
 
-const TMDB_API_KEY = process.env.TMDB_API_KEY;
-const TMDB_BASE_URL = process.env.TMDB_BASE_URL;
 const MOVIE_DIR = process.env.MOVIE_DIR;
 
 // verify the required
-if (!TMDB_API_KEY || !TMDB_BASE_URL || !MOVIE_DIR){
-    console.error("missing required environment vars.");
+if (!MOVIE_DIR){
+    console.error("missing required environment var: MOVIE_DIR");
     process.exit(1);
 }
 
 // test output
-console.log("TMDb API Key: ", TMDB_API_KEY);
-console.log("TMDb Base Url: ", TMDB_BASE_URL);
 console.log("movies path: ", MOVIE_DIR);
 
 (async function main() {
-    try{
-        console.log("project setup complete");
-    } catch (error) {
-        console.error("error occurred: ", error.message);
-    }
+    console.log('starting directory traverse for: ${MOVIE_DIR}');
+    await traverseDirectory(
+        MOVIE_DIR,
+        (filePath) => console.log("📄 File:", filePath),
+        (folderPath) => console.log("\uD83D\uDCC1 Folder: ", folderPath)
+    );
+    console.log("finished directory traversal");
 })();
---
+++ src/traversal.js
@@ -0,0 +1,25 @@
+const { error } = require("console");
+const fs = require("fs/promises");
+const path = require("path");
+
+async function traverseDirectory(dir, onFile, onFolder) {
+    try {
+        const items = await fs.readdir(dir, { withFileTypes: true});
+        
+        for ( const item of items ){
+            const fullPath = path.join(dir,item.name);
+
+            if (item.isDirectory()) {
+                if (onFolder) onFolder(fullPath);
+                // recursively traverser subdirectories
+                await traverseDirectory(fullPath, onFile, onFolder);
+            } else if (item.isFile()) {
+                if (onFile) onFile(fullPath);
+            }
+        }
+    } catch (error){
+        console.error('Error readin directory "${dir}": ', error.messages);
+    }    
+}
+
+module.exports = {traverseDirectory};
